92a747b5a30c19b001a25abc2472091f2ec46240
shahzad6077 <alim6077@gmail.com>
2021-10-29 03:45:11 +0500

    :hammer: Updated phoneno field with regex

--- src/components/Header/Form1.js
+++ src/components/Header/Form1.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { usePlacesWidget } from "react-google-autocomplete";
 
 const Form1 = ({ setFirstFormData }) => {
@@ -10,40 +10,68 @@ const Form1 = ({ setFirstFormData }) => {
             types: ["address"],
             componentRestrictions: { country: "us" },
         },
-    })
+    });
     const [data, setData] = useState({
         address: "",
         name: "",
-        number: ""
+        number: "",
     });
     const onDataChange = (e) => {
         const { name, value } = e.currentTarget;
-        setData((p) => ({ ...p, [name]: value }));
-    }
-    const isValidForm = Object.values(data).every(str => str.length > 0);
+        if (name === "number") {
+
+            value.length <= 11 && setData((p) => ({ ...p, [name]: value.replace(/\D/g, "") }));
+        } else {
+            console.log("5");
+            setData((p) => ({ ...p, [name]: value }));
+        }
+    };
+
+    const isValidForm = Object.values(data).every((str) => str.length > 0);
 
     return (
         <div className="header">
-            <div className="header__content">
-                <h1 className="header__content-h1">
-                    Want a quick cash offer for your home?
-                </h1>
-                <p className="header__content-p">
-                    Fill up the form to be connected with one of our agents
-                </p>
-            </div>
-            <div className="header__textFields">
-                <input ref={ref} type="text" placeholder="What's the address?" name="address" onChange={onDataChange} />
-                <input type="text" placeholder="and your name?" name="name" onChange={onDataChange} />
-                <input type="tel" placeholder="Phone number" name="number" onChange={onDataChange} />
-                <div className="navbar__btn">
-                    <button className="" onClick={() => setFirstFormData(data)}>
-                        Get Offer!
-                    </button>
+            <form action="#">
+                <div className="header__content">
+                    <h1 className="header__content-h1">
+                        Want a quick cash offer for your home?
+                    </h1>
+                    <p className="header__content-p">
+                        Fill up the form to be connected with one of our agents
+                    </p>
                 </div>
-            </div>
+                <div className="header__textFields">
+                    <input
+                        ref={ref}
+                        type="text"
+                        placeholder="What's the address?"
+                        name="address"
+                        onChange={onDataChange}
+                        value={data["address"]}
+                    />
+                    <input
+                        type="text"
+                        placeholder="and your name?"
+                        name="name"
+                        onChange={onDataChange}
+                        value={data["name"]}
+                    />
+                    <input
+                        type="tel"
+                        placeholder="Phone number"
+                        name="number"
+                        onChange={onDataChange}
+                        value={data["number"]}
+                    />
+                    <div className="navbar__btn">
+                        <button className="" onClick={() => setFirstFormData(data)}>
+                            Get Offer!
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
-    )
-}
+    );
+};
 
-export default Form1
+export default Form1;
--- src/components/Header/Form2.js
+++ src/components/Header/Form2.js
@@ -3,27 +3,29 @@ import React from 'react'
 const Form2 = () => {
     return (
         <div className="headertwo">
-            <div className="headertwo__textFields">
-                <select name="house-condition">
-                    <option value="">What's the Condition of the house?</option>
-                    <option style={{ backgroundColor: "transparent" }} value="volvo">First</option>
-                    <option value="saab">Second</option>
-                </select>
-                <input type="text" placeholder="How many beds?" />
-                <input type="text" placeholder="And how many baths" />
-            </div>
-            <div className="headertwo__textFields">
-                <input
-                    type="text"
-                    placeholder="What's the Condition of the house?"
-                />
-                <input type="text" placeholder="How many beds?" />
-                <input type="text" placeholder="And how many baths" />
-            </div>
+            <form action="#">
+                <div className="headertwo__textFields">
+                    <select name="house-condition">
+                        <option value="">What's the Condition of the house?</option>
+                        <option style={{ backgroundColor: "transparent !important" }} value="volvo">First</option>
+                        <option value="saab">Second</option>
+                    </select>
+                    <input type="text" placeholder="How many beds?" />
+                    <input type="text" placeholder="And how many baths" />
+                </div>
+                <div className="headertwo__textFields">
+                    <input
+                        type="text"
+                        placeholder="What's the Condition of the house?"
+                    />
+                    <input type="text" placeholder="How many beds?" />
+                    <input type="text" placeholder="And how many baths" />
+                </div>
 
-            <button className="headertwo__btn" >
-                Get Offer!
-            </button>
+                <button className="headertwo__btn" >
+                    Get Offer!
+                </button>
+            </form>
         </div>
     )
 }
